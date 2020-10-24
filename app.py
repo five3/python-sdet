dde51b5f3041da704cbed6ca6b5184ad500ff8df
chenxiaowu <chenxiaowu@autohome.com.cn>
2020-10-24 23:30:45 +0800

    更新文档

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def create_app():
 
 
 if __name__ == '__main__':
-    create_app().run(host='0.0.0.0', port=9528, debug=True)
+    create_app().run(host='0.0.0.0', port=9528, debug=True, threaded=True)

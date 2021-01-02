a41545eeb5c3d351af9a5d1530b0eb1c88fe6dc4
chenxiaowu <chenxiaowu@autohome.com.cn>
2021-01-02 17:43:54 +0800

    run as well

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def create_app():
 
 
 if __name__ == '__main__':
-    create_app().run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    create_app().run(host='0.0.0.0', port=9528, debug=True, threaded=True)
